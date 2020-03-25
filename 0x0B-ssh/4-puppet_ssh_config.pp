93927c5ee3f43e902031f2a3508ca06ef03fec53
andres0191 <andres_felipe0191@hotmail.com>
2020-03-25 01:43:42 +0000

    modify name of rules in task 4 and delete ensure

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 # Let’s practice using Puppet to make changes to our configuration file.
 
-file_line { 'do not authenticate password':
-    ensure    => 'present',
+file_line { 'Turn off passwd auth':
     path      => '/etc/ssh/ssh_config',
     line      => 'PasswordAutentication no',
     match     => 'PasswordAutentication yes';
   }
 
-file_line { 'file private password':
+file_line { 'Declare identity file':
     path      => '/etc/ssh/ssh_config',
     line      => 'IdentityFile ~/.ssh/holberton';
 }
-
