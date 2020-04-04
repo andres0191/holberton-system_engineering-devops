db4249dd76be790a4526942563dc437de463209d
andres0191 <andres_felipe0191@hotmail.com>
2020-04-04 00:24:47 -0500

    modify task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,6 @@
-
-exec { 'http config'
-command => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "/http {/a add_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf
- && sudo service nginx start'
+# Just as in task #0, we’d like you to automate the task
+# of creating a custom HTTP header response, but with Puppet
+exec { 'config':
+provider => shell,
+command => 'sudo apt-get update; sudo apt-get -y install nginx; sed -i "/http {/a \ \nadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf; sudo service nginx restart'
 }
