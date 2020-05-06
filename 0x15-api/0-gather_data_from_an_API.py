a38032a33ef84197cb0aeffa33b8ecb46f22f079
andres0191 <andres_felipe0191@hotmail.com>
2020-05-06 19:07:30 +0000

    update task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-""" Write a Python script that, using
-    this REST API, for a given employee
-    ID, returns information about his/her
-    TODO list progress.
+"""Write a Python script that, using this REST API,
+   for a given employee ID, returns information about
+   his/her TODO list progress.
 """
-from urllib import urllib
-from request import request
+import requests
 from sys import argv
-
-def given_employee(id_employee):
-    """ using this REST API, for a given
-        employee ID
-    """
-    dic_json = get("https://jsonplaceholder.typicode.com/users".format(id_employee)).json()
-    name_employee = json["name"]
-    number_of_done_task = 0
-    total_number_of_task = 0
-    task=[]
-    for arg in get("")
-
+if __name__ == "__main__":
+    url_todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1])).json()
+    url_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+    url_todos_true = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}&completed=true'.format(argv[1])).json()
+    name_user = url_user.get('name')
+    number_done = len(url_todos)
+    total_number = len(url_todos_true)
+    print("Employee {} is done with task({}/{}):"
+          .format(name_user, total_number, number_done))
+    for task in url_todos_true:
+        name_task = task.get('title')
+        print("\t{}".format(name_task))
