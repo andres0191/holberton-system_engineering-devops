bf5343e93b87644e53ab2e8cd01066124a34be20
andres0191 <andres_felipe0191@hotmail.com>
2020-05-06 20:15:13 +0000

    update task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,14 +6,17 @@
 import requests
 from sys import argv
 if __name__ == "__main__":
-    url_todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1])).json()
-    url_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
-    url_todos_true = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}&completed=true'.format(argv[1])).json()
+    todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+    url_todos = requests.get(todos.format(argv[1])).json()
+    user = 'https://jsonplaceholder.typicode.com/users/{}'
+    url_user = requests.get(user.format(argv[1])).json()
+    lt = 'https://jsonplaceholder.typicode.com/todos?userId={}&completed=true'
+    true = requests.get(lt.format(argv[1])).json()
     name_user = url_user.get('name')
     number_done = len(url_todos)
-    total_number = len(url_todos_true)
+    total_number = len(true)
     print("Employee {} is done with task({}/{}):"
           .format(name_user, total_number, number_done))
-    for task in url_todos_true:
+    for task in true:
         name_task = task.get('title')
         print("\t{}".format(name_task))
