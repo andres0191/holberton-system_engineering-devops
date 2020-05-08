6f1a7fbc39598d54dd3d69923d8af90394c8c1a3
andres0191 <andres_felipe0191@hotmail.com>
2020-05-08 04:32:54 +0000

    modify task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0,
+   extend your Python script to export
+   data in the CSV format
+"""
+
+if __name__ == '__main__':
+
+    url = 'https://jsonplaceholder.typicode.com/'
+    
